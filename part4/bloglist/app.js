c286776c938b44093a860d9ac827609479f2363e
Greenlucid <greenfirechaos@gmail.com>
2020-11-24 16:40:11 +0100

    part4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,31 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blogRouter')
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(() => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
+
+const app = express()
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/bloglist/controllers/blogRouter.js
@@ -0,0 +1,26 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+blogRouter.get('/', (request, response, next) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+
+module.exports = blogRouter
