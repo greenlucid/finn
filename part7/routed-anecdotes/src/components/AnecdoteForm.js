43a45293076b757e197a8db92470bf60e189e8d8
Greenlucid <greenfirechaos@gmail.com>
2020-12-27 19:06:55 +0100

    part7b

--- part7/routed-anecdotes/src/components/AnecdoteForm.js
+++ part7/routed-anecdotes/src/components/AnecdoteForm.js
@@ -1,28 +1,29 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { useField } from '../hooks'
 
 const AnecdoteForm = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const resetFields = () => {
-    setContent('')
-    setAuthor('')
-    setInfo('')
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     props.createNotification(`New anecdote '
       ${content.length>=25 
-        ? `${content.substring(0,20)}...`
-        : content}' was added`,
+        ? `${content.value.substring(0,20)}...`
+        : content.value}' was added`,
       10
     )
     resetFields()
@@ -34,21 +35,22 @@ const AnecdoteForm = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content.inputtable()} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author.inputtable()} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info.inputtable()} />
         </div>
         <button>create</button>
       </form>
+      <button onClick={resetFields}>reset</button>
+      
     </div>
   )
-
 }
 
 export default AnecdoteForm
